86455d70f7400ccdc2b88fcb613e1f47f3061a37
louisdeck <louiisd@protonmail.com>
2022-05-02 18:59:32 +0200

    check p and q are primes

--- simplersa.py
+++ simplersa.py
@@ -1,13 +1,54 @@
 # Simple RSA implementation, not secure at all!
-
 # (n,e) = public key
 # (d) = private key
 
-'''
-p = 53
-q = 59
-e = 3
-'''
+import sys
+
+e = 65537
+
+def isPrime(n):
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+
+    return True
+
+def keygen():
+    p = int(input("Choose a prime number p (1999): ") or 1999)
+    q = int(input("Choose a prime number q (2011): ") or 2011)
+
+    if(isPrime(p) and isPrime(q)):
+        n = p * q
+        phi = (p-1) * (q-1)
+        d = pow(e,-1,phi)
+        return n, d;
+
+    else: sys.exit("2 primes required, exiting.")
+
+def encrypt(n):
+    m1 = int(input("What message would you like to encrypt? "))
+    c = pow(m1,e,n)
+    return m1,c
+
+def decrypt(c,d,n):
+    m2 = pow(c,d,n)
+    return m2
+
+
+if __name__ == '__main__':
+    n, d = keygen()
+    #print(f"{n=}")
+    #print(f"{d=}")
+
+    m1, c = encrypt(n)
+    print(f"{m1=}")
+    print(f"{c=}")
+
+    m2 = decrypt(c,d,n)
+    print(f"{m2=}")
+    print(f"{m1==m2=}")
+
+
 
 '''
 p = 1999 #2 prime numbers p and q
@@ -27,41 +68,6 @@ print(f"{c=}")
 print(f"{m2=}")
 print(f"{m == m2=}")
 '''
-
-e = 65537
-
-def keygen():
-	p = int(input("Choose a prime number p (1999): ") or 1999)
-	q = int(input("Choose a prime number q (2011): ") or 2011)
-	n = p * q
-	phi = (p-1) * (q-1)
-	d = pow(e,-1,phi)
-
-	return n, d;
-
-def encrypt(n):
-	m1 = int(input("What message would you like to encrypt? "))
-	c = pow(m1,e,n)
-	return m1,c
-
-def decrypt(c,d,n):
-	m2 = pow(c,d,n)
-	return m2
-
-
-if __name__ == '__main__':
-	n, d = keygen()
-	#print(f"{n=}")
-	#print(f"{d=}")
-
-	m1, c = encrypt(n)
-	print(f"{m1=}")
-	print(f"{c=}")
-
-	m2 = decrypt(c,d,n)
-	print(f"{m2=}")
-	print(f"{m1 == m2=}")
-
 
 
 
